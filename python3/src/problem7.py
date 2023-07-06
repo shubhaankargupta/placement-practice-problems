37afa2c9dcccc6c380c807a79aa9d06e48c7af71
Shubhaankar Gupta <guptashubhankar06@gmail.com>
2023-07-06 12:21:41 +0530

    Update problem7.py

--- python3/src/problem7.py
+++ python3/src/problem7.py
@@ -3,7 +3,6 @@ Distribution file of the fill problem.
 '''
 
 from typing import List
-
 def fill(array: List[int], i: int, x: int) -> None:
     '''
     Performs a fill operation on the list, replacing all continuous elements 
@@ -20,4 +19,22 @@ def fill(array: List[int], i: int, x: int) -> None:
         None
     '''
     # TODO: Implement this function.
+
+
+    v = array[i]
+    
+    if(i<0 or i>=len(array)):
+        print("i<0 or i>=len(array)")
+        return array
+    if(array[i]!=v):
+        print("array[i]!=v")
+        return array
+    if(array[i]==x):
+        print("array[i]==x")
+        return array
+    else:
+        array[i]=x
+        fill(array,i-1,x)
+        return(fill(array,i+1,x))
     pass
+print(fill([7,2,2,1,1,1,1,2,3,5],4,0))
